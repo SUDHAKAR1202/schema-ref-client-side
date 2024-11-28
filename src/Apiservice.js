efdb73ac78ef2c6b0b8b441ccb30117c1348b555
SUDHAKAR1202 <sudhakargmk12@gmail.com>
2024-11-28 16:19:31 +0530

    added backend deployed url

--- src/Apiservice.js
+++ src/Apiservice.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000'});
+const API = axios.create({ baseURL: 'https://schema-ref-server-side.vercel.app'});
 
 export const createUser = (data) => API.post('/users', data);
 export const createPost = (data) => API.post('/posts', data);
